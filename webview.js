c2261d31075eeb4a27f87d96b5637e4a700d4d8c
Kan Fushihara <kan.fushihara@gmail.com>
2016-11-22 14:25:17 +0900

    remove jquery dependency

--- webview.js
+++ webview.js
@@ -3,24 +3,16 @@ module.exports = (Franz, options) => {
         location.reload();
     }, 90000);
 
-    const script = document.createElement('script');
-    script.setAttribute('src', 'https://code.jquery.com/jquery-3.1.0.min.js');
-    script.setAttribute('integrity', 'sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s=');
-    script.setAttribute('crossorigin', 'anonymous');
-
-    const head = document.getElementsByTagName('head')[0];
-    head.appendChild(script);
-
-    window.onscroll = function(ev) {
-        const entry = $('.entry-block')[0];
+    window.addEventListener('scroll', (ev) => {
+        const entry = document.querySelector('.entry-block');
         if (entry) {
             localStorage.setItem("Franz.HatenaB.latest", entry.getAttribute('data-eid'));
         }
-    };
+    });
 
     function getMessages() {
         const latest = localStorage.getItem("Franz.HatenaB.latest");
-        const entry = $('.entry-block')[0];
+        const entry = document.querySelector('.entry-block');
         if (!entry) {
             return;
         }
@@ -33,7 +25,7 @@ module.exports = (Franz, options) => {
             unread = 1;
         }
         var reply = 0;
-        if ($('.notify-count')[0].style.display != "none") {
+        if (document.querySelector('.notify-count').style.display != "none") {
             reply = parseInt($('.notify-count')[0].innerText);
         }
 
